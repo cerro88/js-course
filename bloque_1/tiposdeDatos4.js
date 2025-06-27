8b14694b5ff96a2f50c65e3fd05cf73e7de17f9e
cerro88 <you@example.com>
2025-06-27 18:11:13 +0200

    teoria tipos

---
+++ bloque_1/tiposdeDatos4.js
@@ -0,0 +1,30 @@
+// Tipos de datos en js
+let num = 2;
+let string = 'Esto es una string';
+let booleano = true;
+
+// orden de elemtos
+let array = [" aa", "bb", 3, (1, 2)];
+console.log(array[0]);
+
+// conjunto de atributos
+let objeto = {
+    nombre: 'Martin',
+    apellido: 'Garcia'
+}
+
+//Una funcion tambien es un tipo de dato
+let funcion = function() {}
+
+//Los tipos en js son dinamicos
+//para comprobar un tipo typeof
+console.log(typeof objeto);
+
+//null y undefined
+//null es un valor nulo (no tiene definido un valor)
+// let valornuelo = null; (es de tipo objeto que indica que eso no tiene valor)
+//nulldefined significa que el valor aun no ha sido definido,
+//quiza a futuro si
+//let valorunderfined
+//let valorunderfined = 5; 
+
